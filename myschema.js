f4054c0f8d490efd02bf1ea80c8bca6eabef4f76
somkiat <somkiat.p@gmail.com>
2016-09-26 23:23:15 +0700

    Create allUser API

--- myschema.js
+++ myschema.js
@@ -1,24 +1,70 @@
 import {
   GraphQLObjectType,
   GraphQLSchema,
-  GraphQLInt
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList
 } from 'graphql/type';
 
-var scores = [1,2,3,4,10];
+var users = [
+  {
+    "id": 1,
+    "firstname": "first name 01",
+    "lastname": "last name 01",
+    "age": 10
+  },
+  {
+    "id": 2,
+    "firstname": "first name 02",
+    "lastname": "last name 02",
+    "age": 20
+  },
+  {
+    "id": 3,
+    "firstname": "first name 03",
+    "lastname": "last name 03",
+    "age": 30
+  }
+];
 
-let schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'getHighestScore',
-    fields: {
-      score: {
-        type: GraphQLInt,
-        description: 'Your Score',
-        resolve: function() {
-          return Math.max.apply(null, scores);
+var UserType = new GraphQLObjectType({
+  name: 'user',
+  fields: function () {
+    return {
+      id: {
+        type: GraphQLInt
+      },
+      firstname: {
+        type: GraphQLString
+      },
+      lastname: {
+        type: GraphQLString
+      },
+      age: {
+        type: GraphQLInt
+      }
+    }
+  }
+});
+
+var queryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: function () {
+    return {
+      allUser: {
+        description: "Get all user",
+        type: new GraphQLList(UserType),
+        resolve: function () {
+          return users;
         }
       }
     }
-  })
+  }
+});
+
+let schema = new GraphQLSchema({
+  query: queryType
 });
 
 export default schema;
